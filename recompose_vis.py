8d94256b9f73874d052f4bfeb7c9bfcb2ecabf7b
nihalsid <yawarnihal@gmail.com>
2021-10-06 21:10:18 +0200

    different targets for shapenet and others

--- recompose_vis.py
+++ recompose_vis.py
@@ -14,7 +14,7 @@ def get_scenes_chunk_dict(base_path, suffix):
 
 def recompose_scene(base_path, chunks, suffix, shift):
     xyz = np.array([[int(y) for y in x.split("__")[-1].split("_")] for x in chunks])
-    meshes = [trimesh.load(base_path / (chunk + suffix)) for chunk in chunks]
+    meshes = [trimesh.load(base_path / (chunk + suffix), force='mesh') for chunk in chunks]
     non_empty_meshes = [type(x) == trimesh.Trimesh for x in meshes]
     joining_shift = [np.array([-1, 0, 0]), np.array([0, -1, 0]), np.array([0, 0, -1])]
     for i in range(len(meshes)):
@@ -22,13 +22,15 @@ def recompose_scene(base_path, chunks, suffix, shift):
             meshes[i].apply_translation(xyz[i, :])
             for j in range(3):
                 if xyz[i, j] != 0:
-                    meshes[i].apply_translation(joining_shift[j] * (xyz[i, j] // 64))
+                    meshes[i].apply_translation(joining_shift[j] * (xyz[i, j] // 64)) 
     if np.array(non_empty_meshes).any():
         try:
+            meshes = [m for m in meshes if type(m) == trimesh.Trimesh]
             concat_mesh = trimesh.util.concatenate(meshes)
             concat_mesh.apply_translation(shift)
             return concat_mesh
         except Exception as e:
+            print("Exception: ", e)
             return None
     else:
         return None
@@ -40,6 +42,7 @@ def recompose_chunks_to_scenes(base_path, suffix, shift):
         rescene = recompose_scene(base_path, scenes_chunk_dict[scene], suffix, shift)
         if rescene is not None:
             rescene.export(base_path / (scene + ".obj"))
+            print('exported')
 
 
 
